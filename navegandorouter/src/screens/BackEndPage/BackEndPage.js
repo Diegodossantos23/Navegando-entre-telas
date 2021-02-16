684ede102b2e61a7f2eea3427aed599bc7ce9778
Diegodossantos23 <diegodossantos900@outlook.com>
2021-02-15 22:38:46 -0300

    instalei as dependencias e criei os componentes

---
+++ navegandorouter/src/screens/BackEndPage/BackEndPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const BackEndPage = () => {
+    return(
+        <div>
+            BackEndPage
+        </div>
+    )
+}
+export default BackEndPage
---
+++ navegandorouter/src/screens/FrontEndPage/FrontEndPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const FrontEndPage = () => {
+    return(
+        <div>
+            FrontEndPage
+        </div>
+    )
+}
+export default FrontEndPage
